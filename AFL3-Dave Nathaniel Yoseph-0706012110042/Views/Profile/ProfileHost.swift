3398f39ff62ae98145b6993de43ba87c5564b30b
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2023-04-24 18:14:35 +0700

    Chapter 3 part 2 Section 4 DONE

--- AFL3-Dave Nathaniel Yoseph-0706012110042/Views/Profile/ProfileHost.swift
+++ AFL3-Dave Nathaniel Yoseph-0706012110042/Views/Profile/ProfileHost.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct ProfileHost: View {
     @Environment(\.editMode) var editMode
-//    SwiftUI provides storage in the environment for values you can access using the @Environment property wrapper. Access the editMode value to read or write the edit scope.
+    //    SwiftUI provides storage in the environment for values you can access using the @Environment property wrapper. Access the editMode value to read or write the edit scope.
     @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
-//        Text("Profile for: \(draftProfile.username)")
+        //        Text("Profile for: \(draftProfile.username)")
         VStack(alignment: .leading, spacing: 10) {
             HStack {
+                if editMode?.wrappedValue == .active{
+                    Button("Cancel", role: .cancel){
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 EditButton()
             }
@@ -25,6 +31,14 @@ struct ProfileHost: View {
                 ProfileSummary(profile: modelData.profile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear(){
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear(){
+                        modelData.profile = draftProfile
+                    }
+                //                Apply the onAppear(perform:) and onDisappear(perform:) modifiers to populate the editor with the correct profile data and update the persistent profile when the user taps the Done button.
+                //                Otherwise, the old values appear the next time edit mode activates.
             }
         }
         .padding()
